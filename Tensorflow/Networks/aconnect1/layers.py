6d290cd937e82ece6cfdd3f6136e7d69c886d786
Andres Felipe Centeno Ochoa <83229099+ACenteno21@users.noreply.github.com>
2022-02-17 22:57:58 -0500

    update

--- Tensorflow/Networks/aconnect1/layers.py
+++ Tensorflow/Networks/aconnect1/layers.py
@@ -663,11 +663,12 @@ def Quant_custom(x,self):
         xi = tf.cast(x,tf.dtypes.float32)
         xMin = tf.math.abs(tf.math.reduce_min(xi))
         xMax = tf.math.abs(tf.math.reduce_max(xi))
+        limit = tf.math.reduce_max(tf.concat([xMin,xMax]))
         #xq = tf.quantization.fake_quant_with_min_max_vars(inputs=xi,min=xMin,max=xMax,num_bits=bwidth)
-        if (xMin.numpy() >= xMax.numpy()):
-            limit = xMin
-        else:
-            limit = xMax
+        #if (xMin.numpy() >= xMax.numpy()):
+        #    limit = xMin
+        #else:
+        #    limit = xMax
         xq = (tf.clip_by_value(tf.floor((x/limit)*(2**(bwidth-1))+1),-(2**(bwidth-1)-1), 2**(bwidth-1)) -0.5)*(2/(2**bwidth-1))*limit
         y = tf.cast(xq,self.d_type)
 
