10dab53ef851e818444d52986d92be8aa769c79f
Andres Felipe Centeno Ochoa <83229099+ACenteno21@users.noreply.github.com>
2022-02-19 15:52:17 -0500

    probe limit = 2xstd

--- Tensorflow/Networks/aconnect1/layers.py
+++ Tensorflow/Networks/aconnect1/layers.py
@@ -643,7 +643,7 @@ def Quant_custom(x,self):
     elif x.name == "W" or x.name == "kernel":
         bwidth = self.bw[0]
         xStd = tf.math.reduce_std(x)
-        limit = (xStd)
+        limit = (2*xStd)
     else:
         bwidth = self.bw[0]
         limit = math.sqrt(6/x.get_shape()[0])
