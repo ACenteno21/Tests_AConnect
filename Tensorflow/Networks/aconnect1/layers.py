49d4e68777646f9df79a2ea78506870bc82c37b3
Andres Felipe Centeno Ochoa <83229099+ACenteno21@users.noreply.github.com>
2022-02-20 15:52:00 -0500

    limit = sqrt(6/288)

--- Tensorflow/Networks/aconnect1/layers.py
+++ Tensorflow/Networks/aconnect1/layers.py
@@ -659,9 +659,9 @@ def Quant_custom(x,self):
         else:
             Pre_limit = math.sqrt(6/(x.get_shape()[0]+x.get_shape()[1]))
 
-        limit = math.sqrt(6/9)
-        if Pre_limit < (limit/3):
-            limit = limit/3
+        limit = math.sqrt(6/288)
+        #if Pre_limit < (limit/3):
+            #limit = limit/3
 
         xStd = tf.math.reduce_std(x)
         #xMean = tf.math.reduce_mean(x)
