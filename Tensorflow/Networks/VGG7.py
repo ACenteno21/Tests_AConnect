7c197aea1af30bb549532f8ce8947492f65104d8
Andres Felipe Centeno Ochoa <83229099+ACenteno21@users.noreply.github.com>
2022-02-21 18:23:24 -0500

    update

--- Tensorflow/Networks/VGG7.py
+++ Tensorflow/Networks/VGG7.py
@@ -20,7 +20,7 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0,
                         InputLayer(input_shape=(32,32,3)),
                         tf.keras.layers.experimental.preprocessing.Resizing(Xsz,Xsz),
                         ## Data augmentation layers
-                        #RandomRotation(1/24),   # Rotate randomly the image 15°
+                        RandomRotation(1/24),   # Rotate randomly the image 15°
                         RandomFlip("horizontal"),
                         RandomTranslation(0.1,0.1),
                         RandomZoom(0.2),
@@ -52,6 +52,7 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0,
                         InputLayer(input_shape=[32,32,3]),
                         tf.keras.layers.experimental.preprocessing.Resizing(Xsz,Xsz),
                         ## Data augmentation layers
+                        RandomRotation(1/24), 
                         RandomFlip("horizontal"),
                         RandomTranslation(0.1,0.1),
                         RandomZoom(0.2),
@@ -83,7 +84,6 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0,
                         BatchNormalization(),
                         #Dropout(0.1),
                         FC_AConnect(10,Wstd=Wstd,Bstd=Bstd,errDistr=errDistr,pool=FC_pool,isQuant=isQuant,bw=bw,weights_regularizer = tf.keras.regularizers.l2(0.0005)),
-                        BatchNormalization(),
                         Softmax()
             ])
 
