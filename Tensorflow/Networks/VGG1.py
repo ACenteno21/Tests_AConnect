8f4639fd29b79acb31ce332f91857a143bddef45
Andres Felipe Centeno Ochoa <83229099+ACenteno21@users.noreply.github.com>
2022-02-20 19:48:54 -0500

    limit new probe

--- Tensorflow/Networks/VGG1.py
+++ Tensorflow/Networks/VGG1.py
@@ -1,11 +1,11 @@
 """
 Script for training and testing VGG-16 with A-Connect
 INSTRUCTIONS:
-Due to the memory usage we recommend to uncomment the first train the model and save it. Then just comment the training 
+Due to the memory usage we recommend to uncomment the first train the model and save it. Then just comment the training
 stage and then load the model to test it using the Monte Carlo simulation.
 """
 import tensorflow as tf
-from aconnect1.layers import Conv_AConnect, FC_AConnect 
+from aconnect1.layers import Conv_AConnect, FC_AConnect
 #from aconnect.layers import Conv_AConnect, FC_AConnect
 from tensorflow.keras.layers import InputLayer, Conv2D, Dense, MaxPool2D, Flatten, RandomFlip, RandomRotation, RandomZoom
 from tensorflow.keras.layers import BatchNormalization, Dropout, ReLU, Softmax, Rescaling, RandomTranslation, RandomCrop
@@ -14,12 +14,13 @@ Xsz = 32
 def model_creation(isAConnect=False,Wstd=0,Bstd=0,
         Conv_pool=16,FC_pool=16,errDistr="normal",
         isQuant=['no','no'],bw=[8,8],isBin="yes"):
-        
+
         if(not(isAConnect)):
                 model = tf.keras.models.Sequential([
                         InputLayer(input_shape=(32,32,3)),
-                        tf.keras.layers.experimental.preprocessing.Resizing(Xsz,Xsz),  
+                        tf.keras.layers.experimental.preprocessing.Resizing(Xsz,Xsz),
                         ## Data augmentation layers
+                        #RandomRotation(1/24),   # Rotate randomly the image 15°
                         RandomFlip("horizontal"),
                         RandomTranslation(0.1,0.1),
                         RandomZoom(0.2),
@@ -41,14 +42,14 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0,
                         BatchNormalization(),
                         MaxPool2D(pool_size=(2,2),strides=(2,2),padding="same"),
                         Conv2D(filters=512,kernel_size=(3,3),strides=(1,1),activation='relu',padding="same"),
-                        BatchNormalization(), 
+                        BatchNormalization(),
                         Conv2D(filters=512,kernel_size=(3,3),strides=(1,1),activation='relu',padding="same"),
                         BatchNormalization(),
                         Conv2D(filters=512,kernel_size=(3,3),strides=(1,1),activation='relu',padding="same"),
                         BatchNormalization(),
                         MaxPool2D(pool_size=(2,2),strides=(2,2)),
                         Conv2D(filters=512,kernel_size=(3,3),strides=(1,1),activation='relu',padding="same"),
-                        BatchNormalization(), 
+                        BatchNormalization(),
                         Conv2D(filters=512,kernel_size=(3,3),strides=(1,1),activation='relu',padding="same"),
                         BatchNormalization(),
                         Conv2D(filters=512,kernel_size=(3,3),strides=(1,1),activation='relu',padding="same"),
@@ -65,7 +66,7 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0,
 
                 model = tf.keras.models.Sequential([
                         InputLayer(input_shape=[32,32,3]),
-                        tf.keras.layers.experimental.preprocessing.Resizing(Xsz,Xsz),   
+                        tf.keras.layers.experimental.preprocessing.Resizing(Xsz,Xsz),
                         ## Data augmentation layers
                         RandomFlip("horizontal"),
                         RandomTranslation(0.1,0.1),
@@ -126,5 +127,3 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0,
 
 
         return model
-
-
