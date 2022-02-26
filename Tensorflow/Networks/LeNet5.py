aaf139c69b55c2c2a7349e3617c235b0abaf8d0f
Andres Felipe Centeno Ochoa <83229099+ACenteno21@users.noreply.github.com>
2022-02-26 18:09:43 -0500

    update for train lenet

--- Tensorflow/Networks/LeNet5.py
+++ Tensorflow/Networks/LeNet5.py
@@ -6,7 +6,7 @@
 #sys.path.append(folder)
 #sys.path.append(folder+'/Layers/')
 import tensorflow as tf
-from aconnect1.layers import Conv_AConnect, FC_AConnect
+from aconnect1.layers_v2 import Conv_AConnect, FC_AConnect
 from tensorflow.keras.layers import InputLayer, Conv2D, Dense, MaxPool2D, Flatten, AveragePooling2D, RandomFlip, RandomRotation, RandomTranslation
 from tensorflow.keras.layers import BatchNormalization, Dropout, ReLU, Softmax, Reshape, Activation, RandomZoom
 
@@ -22,22 +22,22 @@ def model_creation(isAConnect=False,Wstd=0,Bstd=0,
             RandomFlip("horizontal"),
             RandomTranslation(0.1,0.1),
             RandomZoom(0.2),
-			Conv2D(6,kernel_size=(5,5),strides=(1,1),padding="valid",activation="tanh",kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
+			Conv2D(6,kernel_size=(5,5),strides=(1,1),padding="valid",activation="tanh"),#,kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
                         BatchNormalization(),
 			#Activation('tanh'),
                         AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
-			Conv2D(16,kernel_size=(5,5),strides=(1,1),padding="valid",activation="tanh",kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
+			Conv2D(16,kernel_size=(5,5),strides=(1,1),padding="valid",activation="tanh"),#,kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
                         BatchNormalization(),
 			#Activation('tanh'),
                         AveragePooling2D(pool_size=(2,2),strides=(2,2),padding="valid"),
 			Flatten(),
-			Dense(120,activation="tanh",kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
+			Dense(120,activation="tanh"),#,kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
                         BatchNormalization(),
 			#Activation('tanh'),
-			Dense(84,activation="tanh",kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
+			Dense(84,activation="tanh"),#,kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
                         BatchNormalization(),
 			#Activation('tanh'),
-			Dense(10,kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
+			Dense(10),#,kernel_regularizer = tf.keras.regularizers.l2(1e-4)),
 			Softmax()
 		])
 	else:
